39c5590bd98380329766b608bc11734f2a886599
uniqquej <cjy4477@gmail.com>
2023-08-02 01:57:09 +0900

    앱 모델 정의

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='게시글 작성자')
+    title = models.CharField(max_length=100, verbose_name='게시글 제목')
+    content = models.TextField(verbose_name='게시글 내용')
