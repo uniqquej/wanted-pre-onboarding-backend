cff077aa29e703428e6d600507530f4c34611bde
uniqquej <cjy4477@gmail.com>
2023-08-05 18:30:57 +0900

    feat: 게시글 목록 조회

--- articles/views.py
+++ articles/views.py
@@ -1,7 +1,9 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
+from articles.models import Article
 from articles.serializers import ArticleSerializer
 
 class ArticleView(APIView):
@@ -11,3 +13,11 @@ class ArticleView(APIView):
             article_serializer.save(author=request.user)
             return Response(article_serializer.data, status=status.HTTP_201_CREATED)
         return Response(article_serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    def get(self, request):
+        articles = Article.objects.all()
+        
+        if len(articles)==0:
+            return Response('게시글이 없습니다.', status=status.HTTP_200_OK)
+        article_serializer = ArticleSerializer(articles, many=True)
+        return Response(article_serializer.data, status=status.HTTP_200_OK)
